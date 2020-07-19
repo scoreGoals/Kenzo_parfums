d3d5f2f4a6e6db22696d852b3d4887dc1e0fd91d
scoreGoals <42836914+scoreGoals@users.noreply.github.com>
2020-07-19 16:34:17 +0300

    Update reveal.js

--- js/reveal.js
+++ js/reveal.js
@@ -8,7 +8,7 @@ new ScrollMagic.Scene({
         triggerElement: "#kp-imagination",
         triggerHook: 0.9, // show, when scrolled 10% into view
         // duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-        offset: 50 // move trigger to center of element
+        offset: 0 // move trigger to center of element
     })
     .setClassToggle("#kp-face-1", "visible") // add class to reveal
     .addIndicators() // add indicators (requires plugin)
@@ -19,7 +19,7 @@ new ScrollMagic.Scene({
         triggerElement: "#kp-youth",
         triggerHook: 0.9, // show, when scrolled 10% into view
         // duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-        offset: 50 // move trigger to center of element
+        offset: 0 // move trigger to center of element
     })
     .setClassToggle("#kp-energy", "visible") // add class to reveal
     .addIndicators() // add indicators (requires plugin)
@@ -29,7 +29,7 @@ new ScrollMagic.Scene({
         triggerElement: "#kp-new",
         triggerHook: 0.9, // show, when scrolled 10% into view
         // duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-        offset: 50 // move trigger to center of element
+        offset: 0 // move trigger to center of element
     })
     .setClassToggle("#kp-face-2", "visible") // add class to reveal
     .addIndicators() // add indicators (requires plugin)
@@ -40,7 +40,7 @@ new ScrollMagic.Scene({
         triggerElement: "#kp-freedom",
         triggerHook: 0.9, // show, when scrolled 10% into view
         // duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-        offset: 50 // move trigger to center of element
+        offset: 0 // move trigger to center of element
     })
     .setClassToggle("#kp-power", "visible") // add class to reveal
     .addIndicators() // add indicators (requires plugin)
@@ -53,7 +53,7 @@ let revealElements = document.getElementsByClassName("kp-item");
 for (let i = 0; i < revealElements.length; i++) { // create a scene for each element
     new ScrollMagic.Scene({
             triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-            offset: 50, // start a little later
+            offset: 0, // start a little later
             triggerHook: 0.9,
         })
         .setClassToggle(revealElements[i], "visible") // add class toggle
